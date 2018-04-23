d973d506d761c2e1cca2433815fde1014fd0d705
Szczepan Zaskalski <szczepix@goldrapid.pl>
2018-04-23 17:43:29 +0200

    Added example AutoMapperConfig

--- Szkolenie.Techniczne3.Infrastucture/Mappers/AutoMapperConfig.cs
+++ Szkolenie.Techniczne3.Infrastucture/Mappers/AutoMapperConfig.cs
@@ -1,7 +1,17 @@
-﻿namespace Szkolenie.Techniczne3.Infrastucture.Mappers
+﻿using AutoMapper;
+using Szkolenie.Techniczne3.Infrastucture.Dto;
+using SzkolenieTechniczne3.Core.Domain;
+
+namespace Szkolenie.Techniczne3.Infrastucture.Mappers
 {
-    public static class AutoMapperConfig
+/*    public static class AutoMapperConfig
     {
-        
-    }
+        public static IMapper Initialize()
+        => new MapperConfiguration(cfg =>
+        {
+            cfg.CreateMap<UserDto, User>();
+            cfg.CreateMap<User, UserDto>();
+            cfg.CreateMap<Product, ProductDto>().ForMember(x => x.CategoryId, m => m.MapFrom(p => p.CategoryId.Id));
+        }).CreteMapper();        
+    }*/
 }
